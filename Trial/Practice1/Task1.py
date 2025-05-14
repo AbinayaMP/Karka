c33cdcd6c367bfdfe70e140decce0b863bf30991
Abinaya MP <146474512+abinaya7803@users.noreply.github.com>
2025-05-14 14:00:21 +0530

    Task1.py

--- Trial/Practice1/Task1.py
+++ Trial/Practice1/Task1.py
@@ -1,1 +1,61 @@
+#1
 print("Hello,World!")
+#2
+print("Name:Abinaya MP")
+#3
+print("Name:Abinaya MP")
+print("age:21")
+#4
+print("Name:Abinaya MP")
+print("age:"21)
+print("cgpa:"8.10)
+print("isStudent:True")
+#5
+name="Abinaya MP"
+age=21
+print("My name is",name "and my age is",age)
+#6
+a="Name:Abinaya MP"
+b="age:21"
+print(a)
+print(b)
+#7
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+#8
+print("*")
+print("**")
+print("***")
+print("****")
+#9
+name="Abinaya MP"
+age=21
+isStudent=True
+print(name)
+print(age)
+print(isStudent)
+name="Resh"
+age=age+1
+isStudent=False
+print(name)
+print(age)
+print(isStudent)
+#11
+name="Abinaya MP"
+age=21
+date_Of_Birth="07-08-2003"
+height=159.1
+weight=50.10
+degree="BE in ECE"
+gender="Female"
+print(name)
+print(age)
+print(date_Of_Birth)
+print(height)
+print(weight)
+print(degree)
+print(gender)
