7fff9835c5115d717d8b08ace3c7be60ec17dc4e
Abinaya MP <146474512+abinaya7803@users.noreply.github.com>
2025-06-23 22:53:14 +0530

    task

---
+++ AdJSTaskPractice1/Task1.js
@@ -0,0 +1,11 @@
+function sqr(a,b,c,d,e){
+    let f=[a*a,b*b,c*c,d*d,e*e];
+    console.log(f)
+}
+sqr(1,2,3,4,5)
+
+const numbers=[1,2,3,4,5]
+const squares=numbers.map(num => num*num);
+console.log(squares)
+
+
