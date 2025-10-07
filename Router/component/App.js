d045b16a1dd52fcd8bb3cf1e550c313d2f9ba243
Abinaya MP <146474512+AbinayaMP@users.noreply.github.com>
2025-10-07 19:20:44 +0530

    react

---
+++ Router/component/App.js
@@ -0,0 +1,42 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Header from "./component/Header";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./component/Home";
+// import About from "./component/About";
+// import Contact from "./component/Contact";
+
+
+
+
+// function App() {
+//   return (
+//    <>
+//    <BrowserRouter>
+//    <Header />
+//    <Routes>
+//    <Route path='/' element={<Home />} />
+//    <Route path='About' element={<About />} />
+//    <Route path='Contact' element={<Contact />} />
+
+ 
+//    </Routes> 
+//    </BrowserRouter>
+//    </>
+//   );
+// }
+
+// export default App;
+
+
+import Main1 from "./component/Main1";
+
+function App(){
+  return(
+    <>
+    <Main1 />
+    </>
+  )
+
+}
+export default App;
