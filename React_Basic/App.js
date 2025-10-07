87d00eb52f579515416c9ded28785c52ada01c18
Abinaya MP <146474512+AbinayaMP@users.noreply.github.com>
2025-10-07 19:15:45 +0530

    react

---
+++ React_Basic/App.js
@@ -0,0 +1,96 @@
+
+import React from 'react';
+import Props from './components/Props';
+// import Condition from './components/Condition';
+// import Map from './components/Map';
+// import Home from './components/Home';
+// import {Home} from './components/Home';
+// function App() {
+//   return (
+//   <>
+//   <Home />
+//   </>
+//   )
+// }
+
+// export default App;
+
+// import { Greet } from './components/Greet';
+// import User from './components/User';
+// // import image from './assets/image.png'
+// import img1 from './assets/image2.png'
+// import Props from './components/Props';
+// import img2 from './assets/image3.png'
+// import Object from './components/Object'
+// import Spread from './components/Spread';
+// import './App.css
+// import Card from './components/Card';
+// import image from './assets/butterscotch.png';
+// import image1 from './assets/chocochip.png';
+// function App() {
+//   const person={
+//     Name:"karka",
+//     age:21,
+//     place:"nagercoil",
+//     img:"https://th.bing.com/th/id/OIP.-GQXR0XWhXrQH-b8lO3LwAHaHa?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3"
+    
+// }
+// function App(){
+//   let users=[
+//     {
+//       Name:"karka",
+//       age:18,
+//     },
+//     {
+//       Name:"academy",
+//       age:25
+//     }
+//   ]
+
+function App(){
+//   let student=[
+//     {
+//       Name:"Raj",
+//       place:"nagercoil",
+//       is_student:true
+//     },
+//     {
+//       Name:"kavi",
+//       place:"tirunelveli",
+//       is_student:false
+//     },
+//     {
+//       Name:"abi",
+//       place:"trivandrum",
+//       is_student:true
+    // }
+//   ]
+
+
+  return(
+    <>
+    {/* <Greet /> */}
+    {/* <User /> */}
+   {/* <img src={image}style={{height:"500px",width:"500px"}} /> */}
+    {/* <Props name="karka" age={22} place="nagercoil" img={img1}/><br></br> */}
+    {/* <Props name="academy" age={25} place="trivandrum" img={img2}/> */}
+    {/* <Object name={person.Name} age={person.age} place={person.place} img={person.img}/> */}
+        {/* <Object name={person.Name} age={person.age} place={person.place} img={person.img}/> */}
+        {/* <Card img={image} title="ButterScotch IceCream" para="Butterscotch Ice Cream is a smooth, creamy dessert infused with the rich, caramel-like flavor of butterscotch. Made from a blend of brown sugar and butter, it delivers a sweet, buttery taste with hints of toffee. Each spoonful offers a velvety texture and a warm, nostalgic flavor that’s both comforting and indulgent."/><br></br> */}
+        {/* <Card img={image1} title="Chocolate Chip Cookie Dough Ice Cream" para="Chocolate Chip Cookie Dough Ice Cream is a rich and creamy vanilla ice cream blended with chunks of soft, buttery cookie dough and sweet chocolate chips. It offers a delightful mix of smooth, cold creaminess, chewy dough texture, and bursts of chocolatey flavor in every bite."/> */}
+        {/* <Spread {...person}/> */}
+        {/* <Map person={users} /> */}
+        {/* <Condition students={student}/> */}
+
+        <Props name='karka' age='21'/>
+        
+     </>
+  )
+     }
+// }
+  // }
+// } 
+export default App;
+
+
+
