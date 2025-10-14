5a64f449cfbf8cf8e05ec66cd684d6fd5bfe5286
Abinaya MP <146474512+AbinayaMP@users.noreply.github.com>
2025-10-14 14:06:41 +0530

    react

---
+++ ReactTP1/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import Task1 from './component/Task1';
+import Task2 from './component/Task2';
+import Task3 from './component/Task3';
+import Task4 from './component/Task4';
+import Task6 from './component/Task6';
+import Task5 from './component/Task5';
+import Task7 from './component/Task7';
+import Task8 from './component/Task8';
+import Task9 from './component/Task9';
+
+function App() {
+  return (
+    <>
+    {/* <Task1 /> */}
+    {/* <Task2 /> */}
+    {/* <Task3 /> */}
+    {/* <Task4 /> */}
+    {/* <Task5 /> */}
+    {/* <Task6 /> */}
+    {/* <Task7 /> */}
+    {/* <Task8 /> */}
+    <Task9 />
+
+    
+    </>
+  );
+}
+
+export default App;
